2b9e6d0b2005431a1a54ddf0b190e7763ccfa47a
changsoon123 <sool0458@naver.com>
2023-04-26 18:27:22 +0900

    집 가야지 프로젝트 ㄱㄱ

--- src/start/Start_main.java
+++ src/start/Start_main.java
@@ -20,20 +20,27 @@ public class Start_main {
 	        System.out.println("\u001B[31;1;4;41;42;43;44;45;46m----------------------------------------------------\u001B[0m");
 	        
 	        
+	        
 	        System.out.println("1.음식점 찾기");
-	        System.out.println("2.음식점 랭킹");
-	        System.out.println("3.음식점 추가");
-	        System.out.println("4.음식점 삭제");
+	        System.out.println("2.메뉴별 검색");
+	        System.out.println("3.음식점 랭킹");
+	        System.out.println("4.음식점 추가");
+	        System.out.println("5.음식점 삭제");
+	        
+	        
 	        
 //	        System.out.println("1.맛");
 //	        System.out.println("2.거리");
 //	        System.out.println("3.가격");
 //	        System.out.println("4.종합");
 	        
+//	        System.out.println("1.한식");
+//	        System.out.println("2.중식");
+//	        System.out.println("3.일식");
+//	        System.out.println("4.양식");
+	        
 //	        System.out.println("1.좋아요");
 //	        System.out.println("2.리뷰");
-//	        System.out.println("4.종합");
-//	        System.out.println("4.종합");
 	        
 	        
 	}
