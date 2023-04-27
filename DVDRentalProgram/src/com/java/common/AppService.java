dc04b43c584f655b2c915d3c4dfbfc06f150cdcd
changsoon123 <sool0458@naver.com>
2023-04-27 15:26:17 +0900

    전부 삭제

--- DVDRentalProgram/src/com/java/common/AppService.java
+++
@@ -1,7 +0,0 @@
-package com.java.common;
-
-public interface AppService {
-
-	void start();
-	
-}
--- DVDRentalProgram/src/com/java/common/Condition.java
+++
@@ -1,9 +0,0 @@
-package com.java.common;
-
-public enum Condition {
-	PUB_YEAR,
-	NATION,
-	MOVIE_NAME,
-	ALL
-	
-}
